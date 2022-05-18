6d39349097c5b7d64d3b5347acc9fec82fdabc92
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-18 18:32:26 +0530

    Add files via upload

---
+++ fast_strings.py
@@ -0,0 +1,16 @@
+"""
+Name: Karthik
+Date: 18th May 2022
+Day: Wednesday
+Time: 6:00-7:00PM
+File Description:  Fast_Strings
+"""
+Name=input("Enter the name ")
+Empid=int(input("Enter Empid "))
+Salary=float(input("Emter salary "))
+
+# print("The name is %s and Empid is %d"%(Name,Empid))
+# print("The Salary is %.2f"%(Salary))
+
+# print(f"The name is {Name} and Empid is {Empid} ID")
+# print(f"The Salary is ${Salary}")
